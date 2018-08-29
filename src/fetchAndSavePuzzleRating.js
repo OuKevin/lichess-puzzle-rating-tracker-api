89f628108d13e3330ec8e3e26bc0cb666a7a5306
Kevin Ou <therealkevinou@yahoo.com>
2018-08-28 20:46:28 -0500

    Lambda that fetches rating from Lichess and saves it to dynamodb

---
+++ src/fetchAndSavePuzzleRating.js
@@ -0,0 +1,25 @@
+const AWS = require('aws-sdk');
+const superagent = require('superagent');
+
+exports.handler = async () => {
+  try {
+    const { tableName, region, userId } = process.env;
+    const userInfo = await superagent.get(`https://lichess.org/api/user/${userId}`);
+    const { rating } = userInfo.body.perfs.puzzle;
+    const dynamoDocumentClient = new AWS.DynamoDB.DocumentClient({
+      region,
+      apiVersion: '2018-05-21',
+    });
+
+    await dynamoDocumentClient.put({
+      TableName: tableName,
+      Item: {
+        date: String(new Date()),
+        user_id: userId,
+        rating,
+      },
+    }).promise();
+  } catch (error) {
+    console.warn('Error:', error);
+  }
+};
